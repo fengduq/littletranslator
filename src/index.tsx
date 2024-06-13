1d08430f0a4254450192ac9f35ffad2298c65925
fengduq <1640294571@qq.com>
2024-06-13 14:41:07 +0800

    ide test

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './locales/i18n'
+import './locales/i18n';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
